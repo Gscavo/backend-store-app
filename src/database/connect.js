58e80bfd7d91dbcca2dfde265c6fc18c5201f331
gscavo <gsscavo@gmail.com>
2023-10-21 12:24:56 -0300

    Update 1

--- src/database/connect.js
+++ src/database/connect.js
@@ -4,7 +4,8 @@ const connect = async () => {
     try {
         await mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@main.8os1dio.mongodb.net/?retryWrites=true&w=majority`)
     } catch (error) {
-        console.log(error.message);
+        console.log("Ocorreu um erro");
+        console.error(error, error.message);
     } finally {
         console.log("Mongoose Conectado");
     }
